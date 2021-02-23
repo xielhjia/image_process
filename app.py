6abdfa1c19c0acf31c3ab4c17161402ed6ffe7a6
xielihui <sjzxlh@163.com>
2021-02-23 17:50:04 +0800

    first version

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from flask_sockets import Sockets
 import datetime
 import time
@@ -16,7 +16,7 @@ def echo_socket(ws):
 
         now = datetime.datetime.now().isoformat() + 'Z'
         ws.send(now)  #发送数据
-        time.sleep(1)
+        # time.sleep(1)
 
 
 
@@ -24,29 +24,27 @@ def echo_socket(ws):
 # def hello():
 #     return 'Hello World!'
 def index():
-    return render_template('index.html')
+    # return render_template('index.html')
+    return app.send_static_file('index.html')
 
-@app.route('/img/<int:pid>.png')
-def get_image(pid):
-    image_binary = get_image('./img/1.png')
-    # response = make_response(image_binary)
-    # response.headers.set('Content-Type', 'image/jpeg')
-    # response.headers.set(
-    #     'Content-Disposition', 'attachment', filename='%s.jpg' % pid)
-    return image_binary
-    # return '1111111'
+@app.route('/img/<iid>')
+def get_image(iid):
+    print('get image:', iid)
+    return send_file('static/img/'+iid, mimetype='image/png')
 
 def get_image(image_path):
-    img = Image.open(image_path, mode='r')
-    img_byte_arr = io.BytesIO()
-    img.save(img_byte_arr, format='PNG')
-    encoded_img = base64.encodebytes(img_byte_arr.getvalue()).decode('ascii')
-    return encoded_img
+    img = file(image_path)
+    resp = Response(img, mimetype="image/png")
+    return resp
 
 
 if __name__ == "__main__":
-    from gevent import pywsgi
-    from geventwebsocket.handler import WebSocketHandler
-    server = pywsgi.WSGIServer(('127.0.0.1', 5000), app, handler_class=WebSocketHandler)
-    print('server start')
-    server.serve_forever()
+    # from gevent import pywsgi
+    # from geventwebsocket.handler import WebSocketHandler
+    # server = pywsgi.WSGIServer(('127.0.0.1', 5001), app, handler_class=WebSocketHandler)
+    # print('server start')
+    # server.serve_forever()
+
+    app.run(host='127.0.0.1', port=8080, debug=True)
+
+
